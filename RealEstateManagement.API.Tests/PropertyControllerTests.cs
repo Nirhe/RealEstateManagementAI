699a218f4bdd0fe72543a20f0db5d89c461abad4
Nir Heifetz <nir.heifetz@gmail.com>
2025-08-20 23:18:33 -0400

    Unit test project

--- RealEstateManagement.API.Tests/PropertyControllerTests.cs
+++ RealEstateManagement.API.Tests/PropertyControllerTests.cs
@@ -1,64 +1,49 @@
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RealEstateManagement.API.Controllers;
 using RealEstateManagement.API.Data;
 using RealEstateManagement.API.Models;
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace RealEstateManagement.API.Tests;
-
-public class PropertyControllerTests
+namespace RealEstateManagement.API.Tests
 {
-    private static RealEstateContext CreateContext()
+    public class PropertyControllerTests
     {
-        var options = new DbContextOptionsBuilder<RealEstateContext>()
-            .UseInMemoryDatabase(Guid.NewGuid().ToString())
-            .Options;
-        return new RealEstateContext(options);
-    }
+        private DbContextOptions<RealEstateContext> _dbOptions;
 
-    [Fact]
-    public async Task GetById_ExistingProperty_ReturnsProperty()
-    {
-        using var context = CreateContext();
-        var property = new Property { Address = "123 Main St", Bedrooms = 3, MonthlyRent = 1000m, PurchasePrice = 100000m };
-        context.Properties.Add(property);
-        await context.SaveChangesAsync();
-        var controller = new PropertyController(context);
+        public PropertyControllerTests()
+        {
+            _dbOptions = new DbContextOptionsBuilder<RealEstateContext>()
+                .UseInMemoryDatabase(databaseName: System.Guid.NewGuid().ToString())
+                .Options;
+        }
 
-        var result = await controller.GetById(property.Id);
+        [Fact]
+        public async Task GetById_ShouldReturnProperty_WhenPropertyExists()
+        {
+            // Arrange
+            var property = new Property { Id = 1, Address = "123 Main St", Bedrooms = 3, MonthlyRent = 2000, PurchasePrice = 500000 };
+            
+            await using (var context = new RealEstateContext(_dbOptions))
+            {
+                context.Properties.Add(property);
+                await context.SaveChangesAsync();
+            }
 
-        var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        var returned = Assert.IsType<Property>(okResult.Value);
-        Assert.Equal(property.Address, returned.Address);
-    }
+            await using (var context = new RealEstateContext(_dbOptions))
+            {
+                var controller = new PropertyController(context);
 
-    [Fact]
-    public async Task GetById_UnknownProperty_ReturnsNotFound()
-    {
-        using var context = CreateContext();
-        var controller = new PropertyController(context);
+                // Act
+                var result = await controller.GetById(1);
 
-        var result = await controller.GetById(1);
-
-        Assert.IsType<NotFoundResult>(result.Result);
-    }
-
-    [Fact]
-    public async Task Create_ValidProperty_ReturnsCreated()
-    {
-        using var context = CreateContext();
-        var controller = new PropertyController(context);
-        var property = new Property { Address = "456 Elm St", Bedrooms = 2, MonthlyRent = 800m, PurchasePrice = 75000m };
-
-        var result = await controller.Create(property);
-
-        var created = Assert.IsType<CreatedAtActionResult>(result.Result);
-        var createdProperty = Assert.IsType<Property>(created.Value);
-        Assert.Equal(property.Address, createdProperty.Address);
-        Assert.Equal(1, await context.Properties.CountAsync());
+                // Assert
+                var actionResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+                var returnedProperty = actionResult.Value.Should().BeOfType<Property>().Subject;
+                returnedProperty.Should().BeEquivalentTo(property);
+            }
+        }
     }
 }
